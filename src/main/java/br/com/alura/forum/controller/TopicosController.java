e57caabecb0f9a780e4e55a148411639838b59af
Luiz Gustavo Freitas Pereira <luizpereira.mec@outlook.com>
2021-07-05 17:23:24 -0300

    Validation

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
@@ -27,12 +28,17 @@ public class TopicosController {
 
     @GetMapping
     public List<TopicoDto> lista(String nomeCurso){
-        List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+        List<Topico> topicos = null;
+        if (nomeCurso == null){
+            topicos = topicoRepository.findAll();
+        } else {
+            topicos = topicoRepository.findByCursoNome(nomeCurso);
+        }
         return TopicoDto.converter(topicos);
     }
 
     @PostMapping
-    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder){
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder){
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
         URI uri = uriBuilder.path("/topico/{id}").buildAndExpand(topico.getId()).toUri();
